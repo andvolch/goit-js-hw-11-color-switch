9b7ad26bf5666e8385d4041f7e0f44b4e7103784
Andrey Volchenskiy <andvolch@gmail.com>
2021-08-13 11:40:16 +0300

    add color-switch

--- index.js
+++ index.js
@@ -1,59 +1,42 @@
-// Таймер
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+};
 
 
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-const timer = {
-  start() {
-    const startTime = new Date('Aug 24, 2021');
+function onFonChange() {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+    
+};
 
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = startTime - currentTime;
-      const time = getTimeComponents(deltaTime);
-      updateTimer(time);
+let id = null;
 
-      // console.log(`${days}:${hours}:${mins}:${secs}`);
+function fonChangeStart() {
+    id = setInterval(() => {
+        onFonChange();
+        refs.start.disabled = true;
     }, 1000);
-  },
 };
 
-timer.start();
-
-function pad(value) {
-  return String(value).padStart(2, '0');
+function fonChangeStop() {
+    clearInterval(id);
+    refs.start.disabled = false;
 };
 
-function getTimeComponents(time) {
-  /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
-};
-
-function updateTimer({ days, hours, mins, secs }) {
-  selectorForTimer.textContent = `${days}:${hours}:${mins}:${secs}`;
-};
-
-const selectorForTimer = document.querySelector('#timer-1');
+refs.start.addEventListener('click', fonChangeStart);
+refs.stop.addEventListener('click', fonChangeStop);
